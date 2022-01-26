5c561de4225a34ab96aff6e4f63c936322fc99c4
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-26 17:52:21 +0100

    Update main.tf
    2 bucket

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "google_storage_bucket" "bucket-cloud-demo" {
   name = "cloud-demo-bucket-swagatika"
   location = "europe-west1"
 }
+resource "google_storage_bucket" "bucket-cloud-demo2" {
+  name = "cloud-demo-bucket-swagatika2"
+  location = "us-west2"
+}
