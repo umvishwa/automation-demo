e03d390831ec4b3a5c226a088b93bd795f1b98e8
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-26 20:16:45 +0100

    new file structure
    new file structure

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
-resource "google_storage_bucket" "bucket-cloud-demo" {
-  name = "cloud-demo-bucket-swagatika"
-  location = "europe-west1"
-  force_destroy = true
-}
 resource "google_storage_bucket" "bucket-cloud-demo2" {
   name = "cloud-demo-bucket-swagatika2"
   location = "us-west2"
 }
-resource "google_storage_bucket_object" "picture" {
-  name   = "images"
-  source = "file.txt"
+resource "google_storage_bucket_object" "textfile" {
+  name   = "textfile"
+  source = "../images/file.txt"
   bucket = "cloud-demo-bucket-swagatika2"
 }
