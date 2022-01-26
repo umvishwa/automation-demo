094b026f4d3b3ee67be13689305477a540677520
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-26 17:08:30 +0100

    Update main.tf
    comiiting..

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "bucket" {
+resource "google_storage_bucket" "bucket-cloud-demo" {
   name = "cloud-demo-bucket-random-001156589458422"
   location = "europe-west2"
 }
