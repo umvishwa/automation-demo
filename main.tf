cf72e2b946c3b803053fa7729becad78fa7882fb
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-26 20:29:44 +0100

    Update main.tf
    added

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ resource "google_storage_bucket" "bucket-cloud-demo2" {
   name = "cloud-demo-bucket-swagatika2"
   location = "us-west2"
 }
-data "google_storage_bucket_object" "file" {
-  name   = "images/Umesh_photos.jpg"
+resource "google_storage_bucket_object" "picture" {
+  name   = "photos"
+  source = "../images/Umesh_photos.jpg"
   bucket = "cloud-demo-bucket-swagatika2"
 }
