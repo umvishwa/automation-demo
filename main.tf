c6a3a9b61d6b5d3d6e424d51372a27d4daa0fa74
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-26 18:17:10 +0100

    Update main.tf
    file name

--- main.tf
+++ main.tf
@@ -9,6 +9,6 @@ resource "google_storage_bucket" "bucket-cloud-demo2" {
 }
 resource "google_storage_bucket_object" "picture" {
   name   = "images"
-  source = "/automation-demo/images/file.txt"
+  source = "file.txt"
   bucket = "cloud-demo-bucket-swagatika2"
 }
