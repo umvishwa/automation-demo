6c73b9cfcb2c794dfe6c44740a659d885bca20f1
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-26 16:41:59 +0100

    Update main.tf
    bucket location

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
 resource "google_storage_bucket" "bucket" {
   name = "test-bucket-random-001122"
-}
-
-resource "google_storage_bucket" "gcs_bucket" {
-  name = "test-bucket-random-001123"
+  location = "us-west1"
 }
