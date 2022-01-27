3c06488f2479425f1fbccd0162e682eec1522965
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-27 13:09:35 +0100

    Update main.tf
    lets see

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ resource "google_storage_bucket" "bucket" {
 }
 
 resource "google_storage_bucket_object" "archive" {
-  name   = "cloud-fn-demo.zip"
+  name   = "Archive.zip"
   bucket = google_storage_bucket.bucket.name
-  source = "/home/runner/work/automation-demo/automation-demo/cloud-fn-demo/main.py"
+  source = "/home/runner/work/automation-demo/automation-demo/cloud-fn-demo/Archive.zip"
 }
 
 resource "google_cloudfunctions_function" "function" {
   name        = "cloud-fn-demo"
   description = "My function"
-  runtime     = "python39"
+  runtime     = "python38"
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
