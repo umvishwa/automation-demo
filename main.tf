74c46c8c13c499b7c84e4c2e01f97636f0ff00aa
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-27 09:02:05 +0100

    Update main.tf
    pathname

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ resource "google_storage_bucket" "bucket-cloud-demo2" {
 
 resource "google_storage_bucket_object" "picture" {
   name   = "photos"
-  source = "/automation-demo/images/Umesh_photos.jpg"
+  source = "./automation-demo/images/Umesh_photos.jpg"
   bucket = "cloud-demo-bucket-swagatika2"
 }
