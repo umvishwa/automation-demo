0e46b52915c7c22e142ce33756803fb874e10c33
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-27 09:52:15 +0100

    Update main.tf
    fn

--- main.tf
+++ main.tf
@@ -1,10 +1,43 @@
 resource "google_storage_bucket" "bucket-cloud-demo2" {
-  name = "cloud-demo-bucket-swagatika2"
+  name = "cloud-demo-bucket-umvishwa"
   location = "us-west2"
 }
 
 resource "google_storage_bucket_object" "picture" {
   name   = "Umesh_photos.jpg"
   source = "./automation-demo/images/Umesh_photos.jpg"
-  bucket = "cloud-demo-bucket-swagatika2"
+  bucket = "cloud-demo-bucket-umvishwa"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "temp-bucket0101"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "cloud-fn-demo.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./automation-demo/cloud-fn-demo"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "cloud-fn-demo"
+  description = "My function"
+  runtime     = "nodejs14"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "helloGET"
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
 }
