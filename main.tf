57f0bc5670c9dc04555d5367102f021debe54133
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-27 15:28:01 +0100

    Update main.tf
    ffgd

--- main.tf
+++ main.tf
@@ -16,15 +16,15 @@ resource "google_storage_bucket_object" "archive" {
 }
 
 resource "google_storage_bucket_object" "archive" {
-  name   = "main.py"
+  name   = "Archive.zip"
   bucket = google_storage_bucket.bucket.name
-  source = "/home/runner/work/automation-demo/automation-demo/cloud-fn-demo/main.py"
+  source = "/home/runner/work/automation-demo/automation-demo/cloud-fn-demo/Archive.zip"
 }
 
 resource "google_cloudfunctions_function" "function" {
-  name        = "cloud-fn-demo"
+  name        = "fn-cloud-demo"
   description = "My function"
-  runtime     = "python38"
+  runtime     = "python37"
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
