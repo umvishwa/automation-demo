1f4c2649834761a3b8ccc7e16ea539ca3bc782ea
Umesh Vishwakarma <umesh2360@gmail.com>
2022-01-27 15:52:37 +0100

    Update main.tf
    build_environment_variables = "Archive/main.py"

--- main.tf
+++ main.tf
@@ -26,17 +26,10 @@ resource "google_cloudfunctions_function" "function" {
   description = "My function"
   runtime     = "python37"
   available_memory_mb   = 128
+  build_environment_variables = "Archive/main.py"
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
   trigger_http          = true
   entry_point           = "hello_world"
 }
-# IAM entry for all users to invoke the function
-resource "google_cloudfunctions_function_iam_member" "invoker" {
-  project        = google_cloudfunctions_function.function.project
-  region         = google_cloudfunctions_function.function.region
-  cloud_function = google_cloudfunctions_function.function.name
 
-  role   = "roles/cloudfunctions.invoker"
-  member = "allUsers"
-}
