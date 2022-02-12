48f26dc5f7c2a2ce9899a44df491982e7fdb378e
Umesh Vishwakarma <umesh2360@gmail.com>
2022-02-12 21:06:41 +0100

    main

--- main.tf
+++ main.tf
@@ -1,36 +1,4 @@
-/*resource "google_storage_bucket" "bucket" {
-  name     = "cibuild1-bucket-100"
-  location = "europe-west1"
-}
-
-resource "google_storage_bucket_object" "archive" {
-  name   = "Umesh_photos.jpg"
-  bucket = google_storage_bucket.bucket.name
-  source = "/home/runner/work/automation-demo/automation-demo/images/Umesh_photos.jpg"
-}
-*/
-
 resource "google_storage_bucket" "bucket" {
   name     = "temp-bucket01015690"
   location = "europe-west2"
 }
-/*
-resource "google_storage_bucket_object" "archive" {
-  name   = "Archive.zip"
-  bucket = google_storage_bucket.bucket.name
-  source = "/home/runner/work/automation-demo/automation-demo/cloud-fn-demo/Archive.zip"
-}
-
-resource "google_cloudfunctions_function" "function" {
-  name        = "fn-tf-cloud-demo"
-  description = "My function"
-  runtime     = "python39"
-  available_memory_mb   = 128
-  build_environment_variables = {"GOOGLE_FUNCTION_SOURCE"="Archive/main.py"}
-  source_archive_bucket = google_storage_bucket.bucket.name
-  source_archive_object = google_storage_bucket_object.archive.name
-  trigger_http          = true
-  entry_point           = "hello_world"
-}
-*/
-
