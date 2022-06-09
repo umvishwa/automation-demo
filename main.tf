60706b0ed007fa9948eb2a9771397801e91b7d96
Umesh Vishwakarma <umesh2360@gmail.com>
2022-06-09 17:16:59 +0200

    main

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
-# The following example shows how to issue an HTTP GET request supplying
-# an optional request header.
-data "http" "example" {
-  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
-
-  # Optional request headers
-  request_headers = {
-    Accept = "application/json"
+terraform {
+  backend "http" {
+    address = "http://myrest.api.com/foo"
   }
 }
